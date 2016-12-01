c88117290dc2810a3ad16716d9aa88ea52cbe013
Raj-PC\Raj <rajtfib@hotmail.ca>
2016-11-30 20:45:48 -0500

    Added library project files for organizing solution.

---
+++ DistanceCal/Business.BindingContext/Bind.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.BindingContext
+{
+    public class Bind
+    {
+        public Bind()
+        {
+        }
+    }
+}
